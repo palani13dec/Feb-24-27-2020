f24b362b2e99874542d3ea0c783999a78a42e31f
prabhu-durasoft <iamprabhu@yahoo.com>
2020-02-24 12:12:59 +0530

    Day01

---
+++ Day01/src/com/generics/DogFarm.java
@@ -0,0 +1,5 @@
+package com.generics;
+
+public class DogFarm<T extends SomeAnimal<? super MyDog<? extends CrazyDog>>> {
+
+}
--- Day01/src/com/generics/Variance.java
+++ Day01/src/com/generics/Variance.java
@@ -14,6 +14,14 @@ public class Variance {
 		List<? extends Animal> cats = new ArrayList<Cat>();
 		List<? extends Animal> dogs = new ArrayList<Dog>();
 		
+		//Cat cat = new Animal();
+		
+		//Contra-variance
+		List<? super Cat> myAnimals = new ArrayList<Animal>();
+		//myAnimals.add(new Dog());
+		
+		
+		//List<? super Cat> myDogs = new ArrayList<Dog>();
 		
 		//List<? extends Animal> donkeys = new ArrayList<Donkey>();
 		
@@ -30,3 +38,10 @@ class Animal {}
 class Cat extends Animal {}
 class Dog extends Animal {}
 class Donkey {}
+
+
+
+
+
+
+
